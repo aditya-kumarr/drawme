7ac96c7b0529351432764ed01fc1e1412d1d638b
aditya <adityakumar222333@gmail.com>
2023-02-18 10:57:48 +0530

    ✨ New Feature: canvas can be downloaded now

--- src/components/Drawboard.tsx
+++ src/components/Drawboard.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import { Button, ColorInput, ColorPicker, Container, Flex, Input, Paper, Slider } from "@mantine/core";
-import { useInputState, useHotkeys } from "@mantine/hooks";
+import { useInputState, useHotkeys, useScrollLock } from "@mantine/hooks";
 import { MutableRefObject, useEffect, useRef, useState } from "react"
 
 type Path = { color: string, path: Path2D, width: number }[]
@@ -11,10 +11,11 @@ const Drawboard = () => {
     const [isDrawing, setIsDrawing] = useState(false)
     const [colorValue, setColorValue] = useInputState("#000")
     const [paths, setPaths] = useState<Path>([])
-    const [strokeWidth, setStrokeWidth] = useState(10);
+    const [strokeWidth, setStrokeWidth] = useState(4);
     useHotkeys([
         ['ctrl+Z', undo],
     ]);
+    const [scrollLocked, setScrollLocked] = useScrollLock();
 
     useEffect(() => {
         const canvas = canvasRef.current!
@@ -44,6 +45,7 @@ const Drawboard = () => {
     const draw = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
         if (!isDrawing) return
         const { offsetX, offsetY } = nativeEvent;
+        console.log(offsetX, offsetY)
         const path = paths[paths.length - 1].path;
         path.lineTo(offsetX, offsetY)
         contextRef.current.stroke(path)
@@ -64,16 +66,29 @@ const Drawboard = () => {
             return prev.filter((path, i, array) => i !== array.length - 1)
         })
     }
+    function downloadImage() {
+        var link = document.createElement('a');
+        link.download = 'filename.png';
+        link.href = canvasRef.current?.toDataURL()!
+        link.click();
+    }
+    function clear() {
+        setPaths([])
+    }
 
 
     return (<>
         <Container pt="lg">
-            <Flex direction="row" gap="lg">
+            <Flex direction={window.innerWidth > 786 ? "row" : "column"} gap="lg">
                 <Canvas ref={canvasRef} onMouseDown={startDrawing} onMouseMove={draw} onMouseUp={finishDrawing} />
                 <Paper component={Flex} direction="column" gap="lg" justify="center" align="center" >
                     <ColorPicker format="rgba" value={colorValue} onChange={setColorValue} />
                     <Slider w={"100%"} value={strokeWidth} onChange={setStrokeWidth} />
-                    <Button w={100} onClick={undo}>Undo</Button>
+                    <Paper component={Flex} gap="lg" shadow="lg" p="lg">
+                        <Button w={100} onClick={undo}>Undo</Button>
+                        <Button onClick={clear}>clear</Button>
+                        <Button onClick={downloadImage}>Download</Button>
+                    </Paper>
                 </Paper>
             </Flex>
         </Container>
@@ -86,6 +101,7 @@ const Canvas = styled.canvas`
     cursor:pointer;
     width:400px;
     height:400px;
+
 `
 
 export default Drawboard
