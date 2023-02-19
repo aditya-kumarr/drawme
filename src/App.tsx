ab6802e9a9adc035ad0024ebfa6b69ca764a9050
aditya <adityakumar222333@gmail.com>
2023-02-19 07:45:23 +0530

    Merge branch 'https-setup'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import styled from "@emotion/styled"
-import { MantineProvider, Text } from '@mantine/core';
+import { Button, MantineProvider, Text, Container } from '@mantine/core';
 import Drawboard from './components/Drawboard';
 
 export default function App() {
+  const handleNotification = () => {
+    let permissions = Notification.permission
+    if (permissions !== "granted")
+      Notification.requestPermission().then(e => {
+        permissions = e
+      })
+    else {
+      navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        registrations[0].showNotification("hello", {
+          body: "brother...."
+        });
+      });
+
+    }
+  }
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Drawboard />
+      <Container>
+        {/* <Drawboard /> */}
+
+        <Button onClick={handleNotification} >Pop notification</Button>
+      </Container>
     </MantineProvider>
   );
 }
-const Container = styled.div`
-   
-`
