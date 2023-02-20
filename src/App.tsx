9734b87f2948b8da4846065adf783de36f1d5c3e
aditya <adityakumar222333@gmail.com>
2023-02-20 16:00:33 +0530

    🐛 FIX: bought back the drawboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,11 @@ import { Button, MantineProvider, Text, Container } from '@mantine/core';
 import Drawboard from './components/Drawboard';
 
 export default function App() {
-  const handleNotification = () => {
-    let permissions = Notification.permission
-    if (permissions !== "granted")
-      Notification.requestPermission().then(e => {
-        permissions = e
-      })
-    else {
-      navigator.serviceWorker.getRegistrations().then(function (registrations) {
-        registrations[0].showNotification("hello", {
-          body: "brother...."
-        });
-      });
-
-    }
-  }
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Container>
-        {/* <Drawboard /> */}
-
-        <Button onClick={handleNotification} >Pop notification</Button>
-      </Container>
+      {/* <Container> */}
+      <Drawboard />
+      {/* </Container> */}
     </MantineProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import "./index.css"
+import { hideStuffFromMobile } from './utils/HideMobileContext';
 
+hideStuffFromMobile();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
